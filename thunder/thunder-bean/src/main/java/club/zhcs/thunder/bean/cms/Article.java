ca0f8a4b9376dca2710c68449ab286fc26480575
王贵源 <kerbores@gmail.com>
2016-09-18 08:53:24 +0800

    fixed #1

--- thunder/thunder-bean/src/main/java/club/zhcs/thunder/bean/cms/Article.java
+++ thunder/thunder-bean/src/main/java/club/zhcs/thunder/bean/cms/Article.java
@@ -1,5 +1,11 @@
 package club.zhcs.thunder.bean.cms;
 
+import org.nutz.dao.entity.annotation.Column;
+import org.nutz.dao.entity.annotation.Comment;
+import org.nutz.dao.entity.annotation.Name;
+import org.nutz.dao.entity.annotation.Table;
+import org.nutz.lang.random.R;
+
 import club.zhcs.titans.utils.db.po.Entity;
 
 /**
@@ -14,6 +20,17 @@ import club.zhcs.titans.utils.db.po.Entity;
  * @time 2016年9月7日 下午3:54:16
  *
  */
+@Table("t_article")
+@Comment("文章")
 public class Article extends Entity {
+
+	@Name
+	@Column("a_uid")
+	@Comment("文章uuid")
+	private String uid;
+
+	{
+		uid = R.UU16();
+	}
 
 }
