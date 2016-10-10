ee6f31508c9e3107db2e8df84a1b51310bf23d32
kerbores <kerbores@gmail.com>
2016-10-11 07:26:06 +0800

    java.library.path 平台兼容

--- thunder/thunder-web/src/main/java/club/zhcs/thunder/ThunderLauncher.java
+++ thunder/thunder-web/src/main/java/club/zhcs/thunder/ThunderLauncher.java
@@ -1,12 +1,18 @@
 package club.zhcs.thunder;
 
+import org.nutz.lang.Lang;
 import org.nutz.web.WebLauncher;
 
 public class ThunderLauncher {
 
 	public static void main(String[] args) {
 		String path = System.getProperty("java.library.path");
-		System.setProperty("java.library.path", path + ":src/main/webapp/WEB-INF/lib");
+		if (Lang.isWin()) {
+			System.setProperty("java.library.path", path + ";src/main/webapp/WEB-INF/lib");// TODO
+																							// 扩展的路径通过配置加载进来
+		} else {
+			System.setProperty("java.library.path", path + ":src/main/webapp/WEB-INF/lib");
+		}
 		WebLauncher.start(args);
 	}
 }
