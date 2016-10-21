a970a39277b8a17ab411993473d441ef1f9e7677
kerbores <kerbores@gmail.com>
2016-10-21 12:43:20 +0800

    去掉 velocity 演示

--- thunder/thunder-web/src/main/java/club/zhcs/thunder/module/ViewsModule.java
+++ thunder/thunder-web/src/main/java/club/zhcs/thunder/module/ViewsModule.java
@@ -3,11 +3,9 @@ package club.zhcs.thunder.module;
 import org.nutz.mvc.View;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Filters;
-import org.nutz.mvc.annotation.Ok;
 import org.nutz.plugins.view.captcha.CaptchaView;
 
 import club.zhcs.titans.nutz.module.base.AbstractBaseModule;
-import club.zhcs.titans.utils.db.Result;
 
 /**
  * @author Kerbores(kerbores@gmail.com)
@@ -30,11 +28,11 @@ public class ViewsModule extends AbstractBaseModule {
 	 * 
 	 * @return
 	 */
-	@At
-	@Ok("vm:pages/velocity/demo.html")
-	public Result velocity() {
-		return Result.success();
-	}
+	// @At
+	// @Ok("vm:pages/velocity/demo.html")
+	// public Result velocity() {
+	// return Result.success();
+	// }
 
 	/**
 	 * 验证码示例
